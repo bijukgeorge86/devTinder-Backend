36d8d0c8fdcf327b8d42ecd0b899d338dee6b03a
Biju Kattunilathu George <biju@Bijus-MacBook-Pro.local>
2025-02-18 08:04:48 +0530

    Database

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect();
+    await mongoose.connect(
+        "mongodb+srv://bijukgeorge86:zhpiPGXBLqWHvO9j@nodemongodblearn.f2rz0.mongodb.net/devTinder"
+    );
 };
 
 module.exports = connectDB;
